9c19a6afb4a2c65e99b10cf60725ec0549a803f3
Kira <beefkira@gmail.com>
2024-06-27 17:34:05 +0800

    added homepage

--- MainHomepage.php
+++ MainHomepage.php
@@ -4,6 +4,19 @@ $title = "Homepage";
 include 'MainHeader.php';
 
 ?>
+<div class="headerbg">
+    <div class="header-content">
+
+        <p>Explore a world of knowledge with our extensive collection of books. Enjoy exclusive discounts and free shipping for members. Join now and start your reading journey!</p>
+        <a class="btn btn-primary" href="MainLogin.php">Get Started</a>
+    </div>
+</div>
+<div class="main-content">
+    <div class="main-container">
+
+    </div>
+
+</div>
 
 </body>
 
