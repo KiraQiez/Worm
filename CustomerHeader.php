9eb088d54387235ae2daa879aa7b79922475a837
Kira <beefkira@gmail.com>
2024-07-04 14:06:14 +0800

    book rental

--- CustomerHeader.php
+++ CustomerHeader.php
@@ -35,7 +35,7 @@ if ($_SESSION['usertype'] != "customer") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="rsc/bootstrap.min.css">
     <link rel="stylesheet" href="rsc/styles.css">
     <link rel="stylesheet" href="rsc/main.css">
     <link rel="stylesheet" href="rsc/customer.css">
